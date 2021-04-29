007fb4544a0a319c1ecf72bc83ca45f24de45ab4
unknown <sebastianajlds@gmail.com>
2021-04-29 21:55:49 +0100

    corrected features in model, added 1st question in main.py

--- main.py
+++ main.py
@@ -1,26 +1,23 @@
 import tkinter as tk
+from tkinter import *
 
-class Application(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master
-        self.pack()
-        self.create_widgets()
-
-    def create_widgets(self):
-        choices = ['rent', 'sale']
-        variable = StringVar(root)
-        variable.set('rent')
-        w = OptionMenu(root, variable, *choices)
-        self.w.pack(side="top")
-
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=self.master.destroy)
-        self.quit.pack(side="bottom")
-
-    def say_hi(self):
-        print("hi there, everyone!")
 
 root = tk.Tk()
-app = Application(master=root)
-app.mainloop()
+
+canvas = tk.Canvas(root, width= 450, height=550)
+
+#Property size
+L1 = tk.Label(root, text= 'Insert the property size')
+L1.grid(row=0,column=0)
+
+property_size = tk.Entry(root, bd =4)
+property_size.grid(row=0,column=1)
+
+#coordinates
+
+
+#Exit button
+exit_button = tk.Button(root, text="Exit", fg="red", command=root.destroy)
+exit_button.grid(row=1,column=1)
+
+root.mainloop()
