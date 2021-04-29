1457af280f5100709bfb1cf1163b78df7a4690c2
Astrobert0 <sebastianajlds@gmail.com>
2021-04-29 18:16:04 +0100

    main file created

---
+++ main.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.create_widgets()
+
+    def create_widgets(self):
+        choices = ['rent', 'sale']
+        variable = StringVar(root)
+        variable.set('rent')
+        w = OptionMenu(root, variable, *choices)
+        self.w.pack(side="top")
+
+        self.quit = tk.Button(self, text="QUIT", fg="red",
+                              command=self.master.destroy)
+        self.quit.pack(side="bottom")
+
+    def say_hi(self):
+        print("hi there, everyone!")
+
+root = tk.Tk()
+app = Application(master=root)
+app.mainloop()
